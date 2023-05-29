b22d9fdf2c715ec70d6793de2020451e0d2c0376
annyong1 <130875806+annyong1@users.noreply.github.com>
2023-05-28 22:12:49 -0400

    Added the loop for height comparison

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,36 @@
 let pokemonList = [
-  {
-    name: 'Machamp',
-    type: ['fighting'],
-    height: 1.6
-  },
-  {
-    name: 'Magneton',
-    type: ['electric', 'steel'],
-    height: 1
-  },
-  {
-    name: 'Muk',
-    type: ['poison'],
-    height: 1.2
-  },
+    {
+        name: 'Machamp',
+        type: ['fighting'],
+        height: 1.6
+    },
+    {
+        name: 'Magneton',
+        type: ['electric', 'steel'],
+        height: 1
+    },
+    {
+        name: 'Muk',
+        type: ['poison'],
+        height: 1.2
+    },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height >= 1.6) {
+        document.write
+        
+        (pokemonList[i].name + pokemonList[i].height + " - The BIGGEST!");
+
+    } else if (pokemonList[i].height < 1.6 && pokemonList[i].height > 1) {
+        document.write
+        
+        (pokemonList[i].name + pokemonList[i].height + " - The Averagest!");
+
+    } else {
+        document.write
+        
+        (pokemonList[i].name + pokemonList[i].height + "- The smallest!");
+
+    }
+}
