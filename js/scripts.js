2f02ff9ed83b2c9573cf9585faba547f3374c862
annyong1 <130875806+annyong1@users.noreply.github.com>
2023-06-04 21:12:27 -0400

    Trying to implement IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,38 @@
-let pokemonList = [
-    {
-        name: 'Machamp',
-        type: ['fighting'],
-        height: 1.6
-    },
-    {
-        name: 'Magneton',
-        type: ['electric', 'steel'],
-        height: 1
-    },
-    {
-        name: 'Muk',
-        type: ['poison'],
-        height: 1.2
-    },
-];
+let pokemonRepository = (function () {
 
-for (let i = 0; i < pokemonList.length; i++) {
+    let pokemonList = [
+        {
+            name: 'Machamp',
+            type: ['fighting'],
+            height: 1.6
+        },
+        {
+            name: 'Magneton',
+            type: ['electric', 'steel'],
+            height: 1
+        },
+        {
+            name: 'Muk',
+            type: ['poison'],
+            height: 1.2
+        },
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    }
+})();
+
+function listPokemon(pokemon) {
     if (pokemonList[i].height >= 1.6) {
         document.write('</p>');
         document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - The BIGGEST!");
@@ -33,4 +49,25 @@ for (let i = 0; i < pokemonList.length; i++) {
         document.write('</p>');
 
     }
+    
 }
+
+pokemonRepository.getAll().forEach(listPokemon);
+
+// for (let i = 0; i < pokemonList.length; i++) {
+    // if (pokemonList[i].height >= 1.6) {
+    //     document.write('</p>');
+    //     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - The BIGGEST!");
+    //     document.write('</p>');
+
+    // } else if (pokemonList[i].height < 1.6 && pokemonList[i].height > 1) {
+    //     document.write('</p>');
+    //     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - The Averagest!");
+    //     document.write('</p>');
+
+    // } else {
+    //     document.write('</p>');
+    //     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - The smallest!");
+    //     document.write('</p>');
+
+//     }
